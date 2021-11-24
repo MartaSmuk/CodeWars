9dad64874a4a0e92dce0d8b2b25eb75b18b31ff2
MartuCha <msmuk999@gmail.com>
2021-11-24 23:52:38 +0100

    task is finished

--- sum_of_positive.js
+++ sum_of_positive.js
@@ -1,3 +1,16 @@
+
+
 function positiveSum(arr) {
-  
+let sumPosNum = 0;
+for(let i=0; i < arr.length; i++) {
+    if(arr[i] >= 0){
+        sumPosNum += arr[i];
+    } else {
+        continue;
+    }
 }
+return sumPosNum;
+}
+
+console.log(positiveSum([1,-4,7,12]))
+
